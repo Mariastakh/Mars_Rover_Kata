04f9fea773d02c140b1bb07826876aedb42e70ae
Maria Astakhova <32569878+Mariastakh@users.noreply.github.com>
2019-07-23 08:45:55 +0100

    Update rover_spec.rb
    cleaned

--- rover_spec.rb
+++ rover_spec.rb
@@ -22,7 +22,7 @@ describe Rover do
       expect(r.send('r20')).to eq(r.location)
     end
 
-    it "if letter has no value after it, treat as single unit" do
+    it "if letter has no value after it, treats it as a single unit" do
       r = Rover.new
       expect(r.send('fff')).to eq([[10.0, 13.0], 'N'])
     end
@@ -32,12 +32,12 @@ describe Rover do
       expect(r.send('lrl10')).to eq([[10.0, 10.0], 'N'])
     end
 
-    it "wraps values great than 100" do
+    it "wraps values greater than 100" do
       r = Rover.new
       expect(r.send('f90')).to eq([[10.0, 0.0], 'N'])
     end
 
-    it "detects stops if an obstacle is detected" do
+    it "stops if an obstacle is detected" do
       mars = Planet.new
       r = Rover.new(mars)
       expect(r.send('f20b10r140')).to eq([[10.0, 30.0], 'N'])
